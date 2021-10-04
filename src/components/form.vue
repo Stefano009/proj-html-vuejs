ab316cbb08ffb640e3fda488c5a3d51545a026d0
Stefano <stefano.funaro@gmail.com>
2021-10-04 18:08:41 +0200

    form-need-style

--- src/components/form.vue
+++ src/components/form.vue
@@ -1,8 +1,27 @@
 <template>
 <div class="wrapper">
-    <h1>
-        Form
-    </h1>
+    <div class="left">
+        <h3>
+            send a message
+        </h3>
+        <h1>
+            Get in <span>Touch</span>
+        </h1>
+        <p>
+            We will respond to your message as soon as possible.
+        </p>
+            <form>
+                <input type="text" id="fname" name="fname" placeholder="Name">
+                <input type="email" id="email" name="email" placeholder="Email">
+                <input type="tel" id="phone" name="phone" placeholder="Phone">
+                <input type="info" id="info" name="info" placeholder="More Info">
+                <textarea name="text" id="text" cols="50" rows="10"></textarea>
+                <input type="submit" placeholder="Send">
+            </form> 
+    </div>
+    <div class="right">
+
+    </div>
 </div>
 </template>
 
@@ -14,5 +33,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+    .message {
+        height: 200px;
+        width: 300px;
+    }
 </style>
